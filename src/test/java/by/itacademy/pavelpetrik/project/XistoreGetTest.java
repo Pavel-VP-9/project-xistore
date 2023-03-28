5817741d44fc08a0fef4de0715cf47677d169bf8
Pavel-VP-9 <pavelpetrik05241406@gmail.com>
2023-03-28 21:43:45 +0200

    refactor XistoreTest1

---
+++ src/test/java/by/itacademy/pavelpetrik/project/XistoreGetTest.java
@@ -0,0 +1,20 @@
+package by.itacademy.pavelpetrik.project;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+
+public class XistoreGetTest {
+
+    @Test
+    public void testXistore1() {
+
+
+        String url = "https://xistore.by/catalog/irrigatory/irrigator_enchen_mint_3/";
+        when().get(url).
+                then().assertThat().statusCode(200);
+    }
+    }
+
+
+
